717f18c14109258acab7c448c0f791fe9c851655
Mohammad Yusuf <Mohammad.Yusuf@wfp.org>
2022-12-04 21:21:28 +0300

    calculation to db

--- main.py
+++ main.py
@@ -14,10 +14,15 @@ format="%(asctime)s - %(levelname)s - %(message)s")
 # Assign Env to variables to make them easy to call
 usr=os.getenv("USR")
 pwd=os.getenv("PWD")
-idb=os.getenv("IDB")
-ddb=os.getenv("DDB")
+informaitondb=os.getenv("INFODB")
+dedicateddb=os.getenv("DEDICATEDDB")
 srv=os.getenv("SERVER")
-endpoint=os.getenv("ENDPOINT")
+
+country = "AFG"
+startdate= "2022-12-01"
+enddate= "2022-12-01"
+
+endpoint=f"https://api.hungermapdata.org/v1/foodsecurity/country/{country}/region?date_start={startdate}&date_end={enddate}"
 
 #test API connection
 try:
@@ -30,14 +35,21 @@ except:
 finally:
     logging.info("sql connection closed")
 
-mydb = mysql.connector.connect(
+infodb = mysql.connector.connect(
   host=srv,
   user=usr,
   password=pwd,
-  database=idb
+  database=informaitondb
 )
 
-mycursor = mydb.cursor()
+resultdb = mysql.connector.connect(
+  host=srv,
+  user=usr,
+  password=pwd,
+  database=dedicateddb
+)
+
+mycursor = infodb.cursor()
 try:
     mycursor.execute("""select c.country_name,c.adm0_code,c.iso3_code,l.cfii_calcualtion from countries_iso3 as c
 join cfii_list as l
@@ -50,13 +62,42 @@ except:
 finally:
     logging.info("sql connection closed")
 
-
+fulldata = ""
 #get countries list to be calculated
 countries_list = []
 
 for result in myresult:
-    countries_list.append({"adm0":result[1],"iso3" : result[2]})
-print(countries_list[0]['iso3'])
+    country=result[2]
+    endpoint=f"https://api.hungermapdata.org/v1/foodsecurity/country/{country}/region?date_start={startdate}&date_end={enddate}"
+    # countries_list.append(result[1],"iso3" : result[2]})
+    x = requests.get(endpoint)
+    print(endpoint)
+    formatted=x.json()
+    for region in formatted:
+        region_name= region['region']['name']
+        rcsi=region['metrics']['rcsi']['prevalence']
+        fcs=region['metrics']['fcs']['prevalence']
+        cfii=0
+        if fcs>0.5:
+            cfii = (fcs+rcsi) / 2
+        elif fcs<=0.5:
+            cfii=((0.5*fcs) + (1.5 * rcsi))/2
+        else:
+            fcs=0
+        
 
-
-# print(request.text)
+        # print (country,region_name,rcsi,fcs,cfii)
+        try:
+            resultdb.connect()
+            mycursor = resultdb.cursor()
+            val = (country,region_name,rcsi,fcs,cfii)
+            sql = f"INSERT INTO cfii_calculation (country, region,rcsi,fcs,cfii) VALUES ('{country}','{region_name}',{rcsi},{fcs},{cfii})"
+            print (sql)
+            mycursor.execute(sql)
+            resultdb.commit()
+        except:
+            logging.error("sql query failed")
+        finally:
+            mycursor.close()
+            resultdb.close()
+            logging.info("sql connection closed")    
